27d1fc30751c6b37c03835cb0c3e7afdae859e4f
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-23 23:08:50 +0530

    Connection observer
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/main/MainFragment.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/main/MainFragment.kt
@@ -6,14 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.shivamkumarjha.nasapictures.R
 import com.shivamkumarjha.nasapictures.databinding.FragmentMainBinding
+import com.shivamkumarjha.nasapictures.network.ConnectionLiveData
 import com.shivamkumarjha.nasapictures.ui.SharedViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
     //Views
     private lateinit var binding: FragmentMainBinding
 
     private val viewModel: SharedViewModel by activityViewModels()
+
+    @Inject
+    lateinit var connectionLiveData: ConnectionLiveData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,5 +35,15 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observer()
+    }
+
+    private fun observer() {
+        connectionLiveData.observe(viewLifecycleOwner, {
+            if (it)
+                viewModel.getData()
+            else
+                findNavController().navigate(R.id.action_global_offlineDialog)
+        })
     }
 }
