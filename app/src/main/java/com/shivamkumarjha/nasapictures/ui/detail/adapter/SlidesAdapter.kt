48ce79fdee0f2e28759d6a81e818096aaeae578f
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 01:36:03 +0530

    PagerAdapter: Add TextFields
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/detail/adapter/SlidesAdapter.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/detail/adapter/SlidesAdapter.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.Glide
@@ -17,14 +19,20 @@ class SlidesAdapter(private val slides: ArrayList<NASA>) : PagerAdapter() {
     private lateinit var layoutInflater: LayoutInflater
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean =
-        view == `object` as LinearLayout
+        view == `object` as ConstraintLayout
 
     override fun getCount(): Int = slides.size
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         layoutInflater = LayoutInflater.from(container.context)
         val view: View = layoutInflater.inflate(R.layout.item_slide, container, false)
+
         val imageView: ImageView = view.findViewById(R.id.slides_image_view)
+        val title: TextView = view.findViewById(R.id.title_tv)
+        val explanation: TextView = view.findViewById(R.id.explanation_tv)
+        val copyright: TextView = view.findViewById(R.id.copyright_tv)
+        val date: TextView = view.findViewById(R.id.date_tv)
+
         //Loader
         val circularProgressDrawable = CircularProgressDrawable(imageView.context)
         circularProgressDrawable.strokeWidth = 5f
@@ -36,11 +44,18 @@ class SlidesAdapter(private val slides: ArrayList<NASA>) : PagerAdapter() {
             .placeholder(circularProgressDrawable)
             .apply(RequestOptions.centerInsideTransform())
             .into(imageView)
+        //TextFields
+        title.text = slides[position].title
+        explanation.text = slides[position].explanation
+        copyright.text = slides[position].copyright
+        date.text = slides[position].date
+        copyright.isVisible = !slides[position].copyright.isNullOrEmpty()
+
         container.addView(view)
         return view
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as LinearLayout)
+        container.removeView(`object` as ConstraintLayout)
     }
 }
