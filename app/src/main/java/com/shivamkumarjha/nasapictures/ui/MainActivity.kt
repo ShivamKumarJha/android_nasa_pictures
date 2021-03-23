402aba94918de59be851bbbe12ea5389cc1c71e1
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 01:02:24 +0530

    Set AppBarConfiguration
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/MainActivity.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/MainActivity.kt
@@ -3,6 +3,10 @@ package com.shivamkumarjha.nasapictures.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.shivamkumarjha.nasapictures.R
 import com.shivamkumarjha.nasapictures.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,5 +21,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel.getData()
+
+        //AppBar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbarId.setupWithNavController(navController, appBarConfiguration)
     }
 }
