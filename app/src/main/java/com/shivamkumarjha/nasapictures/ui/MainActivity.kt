f3fbf6ca7dca1d8212a75deb54f6713dc341628f
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-23 21:07:33 +0530

    Add nav graph
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/MainActivity.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.shivamkumarjha.nasapictures.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.lifecycleScope
 import com.shivamkumarjha.nasapictures.databinding.ActivityMainBinding
 import com.shivamkumarjha.nasapictures.repository.NASARepository
@@ -16,7 +15,6 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
     //Views
     private lateinit var binding: ActivityMainBinding
-    private lateinit var toolbar: Toolbar
 
     //TODO remove this, added for testing
     @Inject
@@ -26,16 +24,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setViews()
-
         lifecycleScope.launch(Dispatchers.IO) {
             nasaRepository.getNASAData().collect {}
         }
-    }
-
-    private fun setViews() {
-        //Toolbar
-        toolbar = binding.toolbarId
-        setSupportActionBar(toolbar)
     }
 }
