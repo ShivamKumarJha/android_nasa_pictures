d84d25f678cf814acd37e4d7b0266343f0c0a0b8
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 01:07:00 +0530

    Adapter: Glide image loader placeholder
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/main/adapter/NASAAdapter.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/main/adapter/NASAAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.card.MaterialCardView
@@ -39,6 +40,14 @@ class NASAAdapter(private val clickListener: NASAClickListener) :
         private val image: ImageView = itemView.findViewById(R.id.nasa_iv)
         private val title: TextView = itemView.findViewById(R.id.title_tv)
         private val time: TextView = itemView.findViewById(R.id.time_tv)
+        private var circularProgressDrawable: CircularProgressDrawable =
+            CircularProgressDrawable(image.context)
+
+        init {
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+        }
 
         fun bind(position: Int) {
             card.setOnClickListener {
@@ -48,7 +57,7 @@ class NASAAdapter(private val clickListener: NASAClickListener) :
             time.text = nasa[position].date
             Glide.with(image.context)
                 .load(nasa[position].url)
-                .placeholder(R.drawable.ic_image_placeholder)
+                .placeholder(circularProgressDrawable)
                 .apply(RequestOptions.centerCropTransform())
                 .into(image)
         }
