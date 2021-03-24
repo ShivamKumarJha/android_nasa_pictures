fb256c1da2077d06f87a615cde3abf844e74dc22
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 12:44:56 +0530

    ViewModel: Observer LiveData not MutableLiveData
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/nasapictures/ui/SharedViewModel.kt
+++ app/src/main/java/com/shivamkumarjha/nasapictures/ui/SharedViewModel.kt
@@ -1,6 +1,7 @@
 package com.shivamkumarjha.nasapictures.ui
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -14,12 +15,13 @@ import kotlinx.coroutines.launch
 class SharedViewModel @ViewModelInject constructor(
     private val nasaRepository: NASARepository
 ) : ViewModel() {
-    val nasa = MutableLiveData<Resource<List<NASA>?>>()
+    private val _nasa = MutableLiveData<Resource<List<NASA>?>>()
+    val nasa: LiveData<Resource<List<NASA>?>> = _nasa
 
     fun getData() {
         viewModelScope.launch(Dispatchers.IO) {
             nasaRepository.getNASAData().collect {
-                nasa.postValue(it)
+                _nasa.postValue(it)
             }
         }
     }
