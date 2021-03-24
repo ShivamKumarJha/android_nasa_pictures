e912766449563ba98d3d78ceb1fb9844ee0e518e
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 17:19:43 +0530

    Test: Add mock successful response test
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/androidTest/java/com/shivamkumarjha/nasapictures/FileReader.kt
@@ -0,0 +1,23 @@
+package com.shivamkumarjha.nasapictures
+
+import androidx.test.platform.app.InstrumentationRegistry
+import com.shivamkumarjha.nasapictures.ui.BaseApplication
+import java.io.IOException
+import java.io.InputStreamReader
+
+object FileReader {
+    fun readStringFromFile(fileName: String): String {
+        try {
+            val inputStream = (InstrumentationRegistry.getInstrumentation().targetContext
+                .applicationContext as BaseApplication).assets.open(fileName)
+            val builder = StringBuilder()
+            val reader = InputStreamReader(inputStream, "UTF-8")
+            reader.readLines().forEach {
+                builder.append(it)
+            }
+            return builder.toString()
+        } catch (e: IOException) {
+            throw e
+        }
+    }
+}
--- app/src/androidTest/java/com/shivamkumarjha/nasapictures/MainActivityTest.kt
+++ app/src/androidTest/java/com/shivamkumarjha/nasapictures/MainActivityTest.kt
@@ -2,12 +2,18 @@ package com.shivamkumarjha.nasapictures
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jakewharton.espresso.OkHttp3IdlingResource
 import com.shivamkumarjha.nasapictures.ui.MainActivity
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -22,11 +28,28 @@ class MainActivityTest {
     fun setup() {
         ActivityScenario.launch(MainActivity::class.java)
         mockWebServer.start(8080)
+        IdlingRegistry.getInstance().register(
+            OkHttp3IdlingResource.create(
+                "okhttp",
+                OkHttpClient()
+            )
+        )
     }
 
     @Test
     fun onLaunchIsAppBarDisplayed() {
         onView(withId(R.id.appbar_id)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testSuccessfulResponse() {
+        mockWebServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse()
+                    .setResponseCode(200)
+                    .setBody(FileReader.readStringFromFile("success_response.json"))
+            }
+        }
     }
 
     @After
