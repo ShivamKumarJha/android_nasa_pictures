468af1c17da0ce6218b86404b936549f9b588679
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-24 15:53:48 +0530

    Test: Date parse test
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/androidTest/java/com/shivamkumarjha/nasapictures/MainActivityTest.kt
+++ app/src/androidTest/java/com/shivamkumarjha/nasapictures/MainActivityTest.kt
@@ -5,19 +5,13 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.shivamkumarjha.nasapictures.ui.MainActivity
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-
-    @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity> =
-        ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun appLaunchesSuccessfully() {
@@ -27,7 +21,6 @@ class MainActivityTest {
     @Test
     fun onLaunchIsAppBarDisplayed() {
         ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.appbar_id))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.appbar_id)).check(matches(isDisplayed()))
     }
 }
