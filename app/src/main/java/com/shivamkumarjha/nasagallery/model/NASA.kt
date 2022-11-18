a6950f33e0531f04e6c001903b944bb4ee29a917
ShivamKumarJha <jha.shivam3@gmail.com>
2022-11-19 00:03:16 +0530

    Nasa model class
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/nasagallery/model/NASA.kt
@@ -0,0 +1,17 @@
+package com.shivamkumarjha.nasagallery.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "nasa")
+data class NASA(
+    @SerializedName("copyright") val copyright: String?,
+    @SerializedName("date") val date: String,
+    @SerializedName("explanation") val explanation: String,
+    @SerializedName("hdurl") val hdURL: String,
+    @SerializedName("media_type") val media_type: String,
+    @SerializedName("service_version") val serviceVersion: String,
+    @SerializedName("title") val title: String,
+    @PrimaryKey @SerializedName("url") val url: String
+)
