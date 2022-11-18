8fb64c218d9f37c9ebc59da5697a149d21908d61
ShivamKumarJha <jha.shivam3@gmail.com>
2022-11-19 00:01:10 +0530

    Config
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/nasagallery/config/Constants.kt
@@ -0,0 +1,9 @@
+package com.shivamkumarjha.nasagallery.config
+
+object Constants {
+    //API
+    const val BASE_URL = "https://raw.githubusercontent.com/obvious/take-home-exercise-data/trunk/"
+
+    //Database
+    const val DB_NAME = "nasa_gallery.db"
+}
